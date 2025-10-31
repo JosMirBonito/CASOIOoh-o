97a637be0174fafa827a8738135e24542b945391
JosMirsito <josmir2705@hotmail.com>
2025-10-31 15:49:02 -0500

    a

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tienda.views import produccion_view
 
 urlpatterns = [
+    path('', produccion_view, name='home'),
     path('produccion/', produccion_view, name='produccion'),
 ]
